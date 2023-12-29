c40eda0e9ac35c56d9e1b89aa39122d3e73c97fd
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2023-12-29 11:34:27 +0530

    source of file structure

---
+++ Source_File.java
@@ -0,0 +1,25 @@
+/* ++++++++in java that should be any number of classes that should not be fixed 
+ * +++++ Java class contain at most one public class allowed
+ * +++++ More than one public class that can't allowed
+ */
+
+ class A{
+    public static void main(String[] args) {
+        System.out.println("hii");
+    }
+ }
+ class B{
+    public static void main(String[] args) {
+        System.out.println("Hiii 2");
+    }
+ }
+ class C{
+    public static void main(String[] args) {
+        System.out.println("Hii 2");
+    }
+ }
+ public class Source_File {
+    public static void main(String[] args) {
+        
+    }
+ }
