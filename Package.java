090ca6de640494c974e6c41e1e4169d0f6a6acf0
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-01-04 20:58:37 +0530

    important point of package

--- Package.java
+++ Package.java
@@ -1,4 +1,13 @@
 // "java.lang" and "java.default"  these two package are not required
+// package num1;
+// package num2; // in java source file only one package are allowed
+// ++++ package statement is the first statement
+
+
+/*********  IMPORTANT ORDER  ******/
+// first is  "Package statement" ONLY ONE
+// second is "Import statement"  ANY NUMBER
+// third one is "class / interface etc... "  ANY NUMBER
 
 public class Package {
     public static void main(String[] args) {
@@ -8,3 +17,4 @@ public class Package {
         StringBuffer sb= new StringBuffer("Shyam");
     }
 }
+
