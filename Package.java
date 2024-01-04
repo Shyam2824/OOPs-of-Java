5979becf42a4ac1c6c1037ee296b285f6f3fcfc0
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-01-04 18:32:05 +0530

    package

---
+++ Package.java
@@ -0,0 +1,10 @@
+// "java.lang" and "java.default"  these two package are not required
+
+public class Package {
+    public static void main(String[] args) {
+        String s= new String("Shyam");
+        Thread t= new Thread();
+        Exception e =new Exception();
+        StringBuffer sb= new StringBuffer("Shyam");
+    }
+}
