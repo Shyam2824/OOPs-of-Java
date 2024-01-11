d54bf5401c27a54ca5ff328fa5363b03d83c02d4
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-01-11 21:01:31 +0530

    modifire notes

---
+++ class_modifier.java
@@ -0,0 +1,7 @@
+// Modifier describe the behavior of class
+// if class is public ascess any where out side the class and inside the class
+// modifier of top level classes are Public,Default, Abstract,FInal
+// inner class modifier are public, private, protected
+public class class_modifier {
+    
+}
