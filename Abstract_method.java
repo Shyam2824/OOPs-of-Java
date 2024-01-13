df506584bf2b0e6d0434b2da0c4285e52b968b83
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-01-13 21:19:30 +0530

    notes

---
+++ Abstract_method.java
@@ -0,0 +1,8 @@
+// abstract  only class and Method
+// it means not clear meaning
+// it has only declearation but not implementation child class are response to implementation
+// it has no "{}" bracket that valid";"
+
+public class Abstract_method {
+    
+}
