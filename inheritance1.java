7aaaa302991281bcf7ffff5c0d8eed3c4314b4ad
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-01-28 19:41:54 +0530

    notes

--- inheritance1.java
+++ inheritance1.java
@@ -1,4 +1,13 @@
 /****it is the inheriting the properties of parents class into child class. 
+ *  Advantage: Code reusability and achieved polymorphism
+ * 
+ * Types of inheritance: 1. Single inheritance A-->B
+ * 2. Multilevel inheritance A-->B-->C then A-->C
+ * 3. Hierarical inheritance A-->B and A-->C B & C has no relation  one class properties in  multiple class
+ * 4. Multiple Hierarical inheritance B-->A   & C-->A  multiple class properties in one class
+ * 5. Hybrid inheritance : that is connect to all class 
+ *  in java only 3 inheritance : Single , Multiple , Hierical inheritance
+ * ++++++++ Constrctor  and private access modifier cant inherit to other class
  * 
  */
 class Animal{
@@ -13,6 +22,6 @@ public class inheritance1 {
     public static void main(String[] args) {
         Dog d= new Dog();
         d.eat();
-        
+
     }
 }
