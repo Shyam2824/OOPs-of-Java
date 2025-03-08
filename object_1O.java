b06ad9516f780e2f34f4f2e35c279676b00c42cb
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-03-08 22:36:39 +0530

    for loop

---
+++ object_1O.java
@@ -0,0 +1,19 @@
+
+
+
+
+public class object_1O {
+     public static void main(String[] args) {
+        int Sum , a,b;
+        
+        
+        a=20;
+         b=30;
+         Sum=a+b;
+         int Sum1= a-b;
+         int Sum2= a*b;
+        System.out.println(  Sum + " Subtract"+Sum1+" " + Sum2);
+        
+     }
+    
+}
