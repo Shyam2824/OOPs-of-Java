764a41970a8056e72bae9003f4e315be11bb7ed4
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2025-03-08 22:43:26 +0530

    for pattern based

--- practice.java
+++ practice.java
@@ -1,10 +1,11 @@
 class practice{
     public static void main(String[] args) {
         
-        for(int i=1; i<=10; i++){
-            System.out.println(i + "* 7 =" + i*7);
+        for(int i=1, j=1; j<=6; i=i*10 ,j++){
+            // System.out.println(i + "* 7 =" + i*7);
+            // System.out.println(i + "* 18 =" + i*18);
             
-            System.out.println(i + "* 18 =" + i*18);
+            System.out.println(i);
 
         }
     }
